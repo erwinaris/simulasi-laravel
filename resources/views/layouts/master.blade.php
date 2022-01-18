424190a33023a0087b91f154380bef8de724c5c9
erwinaris <erwinaris442@gmail.com>
2022-01-18 12:44:21 +0700

    rev2, tambah type=text/css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
     </main>
 
     {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> --}}
-    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}" type="text/javascript"></script>
 
     <script>
         // Example starter JavaScript for disabling form submissions if there are invalid fields
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.css') }}" type="text/css">
 
 <title>@yield('title') - Erwin Aris Prayogo</title>
